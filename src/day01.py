39bf46719aa4c1c9031a6a0a5932259cd35d94ba
Neil Vass <neil.vass+github@gmail.com>
2023-01-29 21:14:38 +0000

    Day 1 complete

---
+++ src/day01.py
@@ -0,0 +1,26 @@
+def fetch_data(path):
+    with open(path, 'r') as f:
+        return f.readline().rstrip()
+
+def find_floor(data):
+    return data.count('(') - data.count(')')
+
+def pos_for_basement(data):
+    floor = 0
+    for pos, c in enumerate(data, 1):
+        floor += 1 if c == '(' else -1
+        if floor == -1:
+            return pos
+
+
+#--------------------- tests -------------------------#
+
+def test_find_floor():
+    assert find_floor('(())') == 0
+    assert find_floor(')())())') == -3
+
+#-----------------------------------------------------#
+
+if __name__ == "__main__":
+    data = fetch_data('data/day01.txt')
+    print(pos_for_basement(data))
