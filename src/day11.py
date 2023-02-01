f3df5f4c79f51c73e7a1c71ea86efc87ad124c99
Neil Vass <neil.vass+github@gmail.com>
2023-02-01 15:34:50 +0000

    Day 11 complete

--- src/day11.py
+++ src/day11.py
@@ -20,15 +20,43 @@ def valid(password):
             has_no_forbidden_letters(password) and
             has_two_pairs(password))
 
-def find_next(password):
-    stem, last = password[:-1], password[-1]
-    last_idx = string.ascii_lowercase.index(last)
-    for i in range(1, 26):
-        new_password = stem + string.ascii_lowercase[(last_idx+i) % 26]
-        if valid(new_password):
-            return new_password
-    raise Exception('Welp, out of ideas')
 
+
+valid_letters = string.ascii_lowercase
+
+# LOL well I have solved a much harder puzzle than I needed to here
+def _find_next_too_complex(fixed_part, rotating_part, original):
+    if len(rotating_part) == 0:
+        if valid(fixed_part) and fixed_part != original:
+            return fixed_part
+    else:
+        initial_idx = valid_letters.index(rotating_part[0])
+        for i in range(len(valid_letters) +1):
+            next_letter = valid_letters[(initial_idx+i) % len(valid_letters)]
+            valid_password = _find_next(fixed_part + next_letter, rotating_part[1:], original)
+            if valid_password:
+                return valid_password   
+    return None
+
+def _find_next(fixed_part, rotating_part, original):
+    if len(rotating_part) == 0:
+        if valid(fixed_part) and fixed_part != original:
+            return fixed_part
+    else:
+        # Find it without changing this wheel?
+        valid_password = _find_next(fixed_part + rotating_part[0], rotating_part[1:], original)
+        if valid_password:
+                return valid_password 
+        # Nope, start rotating this one.
+        initial_idx = valid_letters.index(rotating_part[0])
+        for next_letter in valid_letters[initial_idx+1:]:
+            valid_password = _find_next(fixed_part + next_letter, (len(rotating_part)-1)*'a', original)
+            if valid_password:
+                return valid_password   
+    return None
+
+def find_next(password):
+    return _find_next(fixed_part='', rotating_part=password, original=password)
 
 #--------------------- tests -------------------------#
 
@@ -38,6 +66,8 @@ def test_valid():
     assert not valid('abbcegjk')
 
 def test_find_next():
+    assert valid('aabbabc')
+    assert find_next('aabbaba') == 'aabbabc'
     assert find_next('abcdefgh') == 'abcdffaa'
     assert find_next('ghijklmn') == 'ghjaabcc'
 
@@ -45,5 +75,6 @@ def test_find_next():
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
-    data = fetch_data('data/day11.txt')
-    print('Hello, World!')
+    new_pass = find_next('<input>')
+    new_pass = find_next(new_pass)
+    print(new_pass)
